1dd7ad1f80e7bdc4fc681e048090daf389cec206
jhndvdgvr <david063095@gmail.com>
2019-01-23 17:35:57 +0800

    done inserting narrative for routine with site not in event

--- src/ChatterBox.php
+++ src/ChatterBox.php
@@ -174,6 +174,7 @@ class ChatterBox implements MessageComponentInterface {
             } else if ($msgType == "sendSmsToRecipients") {
                 $exchanges = $this->chatModel->sendSms($decodedText->recipients,$decodedText->message);
                 $this->chatModel->autoTagMessage($decodedText->sender_id,$exchanges['convo_id'][0],$exchanges['timestamp']);
+                $this->chatModel->routineNarrative($decodedText->site_id,$exchanges['timestamp']);
                 $from->send(json_encode($exchanges));
             } else if ($msgType == "newSmsInbox") {
                 echo "New Incomming SMS Received. Sending data to all WSS clients.\n";
